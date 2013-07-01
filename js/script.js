10a32a44516817cd076565aaa7f022a7567df1b8
Isaac Elias <nicecurelom@gmail.com>
2013-07-01 14:00:57 -0700

    the tiles can be shown, hooray!

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-  console.log(':)')
+  console.log(':)');
+
+  var tiles = $('.tile');
+  var num_clicks = 0;
+
+  $.each(tiles, function(index, tile){
+      var tile = $(tile);
+      $(tile).on('click', handle_click);
+  });
+
+  function handle_click(tile){
+
+    var tile = $(this);
+    tile.addClass('active');
+  }
+
 })
